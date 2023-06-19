f1711b9a33fe9f5ba0d5ad548fc3c63b953992dd
averyden <igo408gt@gmail.com>
2023-06-19 14:32:13 +0200

    Publishing to github repo

---
+++ src/main.js
@@ -0,0 +1,140 @@
+const notesContainer = document.getElementById("app")
+const addButton = notesContainer.querySelector(".add-note")
+const saveUpdateButton = document.querySelector(".save-update")
+const selectedNoteText = document.querySelector(".sidebar li:first-child text")
+
+const menuButton = document.querySelector(".hamburger")
+const sidebar = document.querySelector(".sidebar")
+const deleteButton = document.querySelector(".deletenotebutton")
+
+let currentSelectedNote = null //Make function that gets the ID of the note that the user has clicked and then parse it through the <li><text>You have not selected a note to configure.</text></li> <!--Current selected note:--> element in the HTML document
+
+//this instantly calls the getNotes function, so that the notes are loaded upon startup.
+getNotes().forEach(note => {
+    const noteElement = createNoteElement(note.id, note.content)
+    notesContainer.insertBefore(noteElement, addButton)
+});
+
+addButton.addEventListener("click", () => addNote())
+menuButton.addEventListener("click", toggleSidebar)
+deleteButton.addEventListener("click", () => promptDelete(currentSelectedNote))
+
+function promptDelete() {
+    const selectedNote = getNotes().find((note) => note.id === currentSelectedNote);
+  
+    if (selectedNote) {
+      const doDelete = confirm(`Are you sure you want to delete note: ${selectedNote.id} (${selectedNote.content})?`);
+  
+      if (doDelete) {
+        deleteNote({
+          id: selectedNote.id,
+          noteElement: document.getElementById(selectedNote.id),
+        });
+        currentSelectedNote = null;
+        updateSelectedNoteText();
+      }
+    }
+}
+  
+  
+
+function toggleSidebar() {
+    menuButton.classList.toggle("is-active")
+    sidebar.classList.toggle("is-open")
+    notesContainer.classList.toggle("app--sidebar-open")
+}
+
+function getNotes() {
+    console.log("Getting user notes..")
+    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+}
+
+function saveNotes(notes) {
+    localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+
+}
+
+function createNoteElement(id, content) {
+    const element = document.createElement("textarea");
+
+    element.id = id;
+
+    element.classList.add("note")
+    element.value = content;
+    element.placeholder = "Empty note"
+
+    element.addEventListener("change", () => {
+        updateNote(id, element.value)
+    });
+
+    element.addEventListener("click", () => {
+        currentSelectedNote = id
+        updateSelectedNoteText()
+    })
+
+
+    // This way of deleting a note is stupid. I could try to add a sidebar with more configuration options, but thats for when i get more skilled.
+    /*element.addEventListener("dblclick", () => {
+        const doDelete = confirm("Do you want to delete this note?")
+        
+        if(doDelete) {
+            deleteNote(id, element)
+        }
+    })*/
+
+    return element
+}
+
+function addNote() {
+    const currentNotes = getNotes()
+    const noteObject = {
+        id: Math.floor(Math.random() * 10000),
+        content: ""
+    };
+
+    const noteElement = createNoteElement(noteObject.id, noteObject.content)
+    notesContainer.insertBefore(noteElement, addButton)
+
+    currentNotes.push(noteObject)
+    saveNotes(currentNotes)
+}
+
+function updateNote(id, newContent) {
+    console.log("Updating note...")
+    const currentNotes = getNotes()
+    const selectedNote = currentNotes.filter(currentNotes => currentNotes.id == id)[0]
+
+    selectedNote.content = newContent
+    saveNotes(currentNotes)
+
+    //dunno why the fuck js wants you to use backticks if you wanna format shit but.. if it works.. it works. 🤷‍♀️
+    console.log(`Note ${selectedNote.id} has been updated`)
+}
+
+function deleteNote({ id, noteElement }) {
+    const currentNotes = getNotes().filter((note) => note.id !== id);
+    saveNotes(currentNotes);
+  
+    if (noteElement) {
+      console.log(`deleting note ${noteElement}`);
+      noteElement.classList.add("note--delete")
+      noteElement.addEventListener("transitioned", () => {
+        notesContainer.removeChild(noteElement);
+      })
+    }
+  
+    if (currentSelectedNote === id) {
+      currentSelectedNote = null;
+      updateSelectedNoteText();
+    }
+} 
+
+function updateSelectedNoteText() {
+    const selectedNote = getNotes().find((note) => note.id === currentSelectedNote)
+
+    if (selectedNote) {
+        selectedNoteText.textContent = `Current selected note(${selectedNote.id}): ${selectedNote.content}`
+    } else {
+        selectedNoteText.textContent = "You have not selected a note to configure."
+    }
+}
