cd9bb0d8c3506d74d52740d1b66545592819be62
Averyden <igo408gt@gmail.com>
2024-09-02 08:58:28 +0200

    Philosophical question for later (I NEED TO STOP BLOATING MY COMMIT COUNT)

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -3,3 +3,6 @@ function toggleSidebar() {
     sidebar.classList.toggle("is-open")
     notesContainer.classList.toggle("app--sidebar-open")
 }
+
+//? Is there a reason i made this file just for it to only be this?
+//? Like i feel like I couldve just kept it in index....
