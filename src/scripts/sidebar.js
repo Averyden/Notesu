a8cdc5039ea3d6866314ca54be8e2a27dd760a5c
Averyden <igo408gt@gmail.com>
2025-02-11 18:14:56 +0100

    Replace everything with vue standard files

--- src/scripts/sidebar.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    menuButton.classList.toggle("is-active")
-    sidebar.classList.toggle("is-open")
-    notesContainer.classList.toggle("app--sidebar-open")
-}
-
-//? Is there a reason i made this file just for it to only be this?
-//? Like i feel like I couldve just kept it in index....
